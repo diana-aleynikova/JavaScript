618e76b4c4ec2d4392d310167346cf73222ca35d
Diana Aleynikova <diana-aleynikova@users.noreply.github.com>
2026-01-17 18:41:04 +0000

    update leson 2

---
+++ Lesson2-main/HomeWork/js/script.js
@@ -0,0 +1,103 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// const newValue = prompt("введіть змінну: ");
+// console.log(typeof newValue);
+
+// if (newValue >= 0) {
+//     console.log(Boolean(1));
+// } else {
+//     console.log(Boolean(0));
+// }
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// const test = prompt("введіть змінну: ").toLowerCase();
+// if (test === "test")
+// {
+//     console.log(Boolean(1));
+// } else{
+//     console.log(Boolean(0));
+// }
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// const value = prompt("введіть змінну: ");
+// if (value > 10){
+//     console.log(value - 5);
+// }
+// else if (value < 10) {
+//     console.log(Number(value) + 5);
+// }
+// else {
+//     console.log(value);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const value = prompt("введіть число від 1 до 12: ");
+// switch (Number(value))
+// {
+//     case (1):
+//         console.log("January");
+//         break;
+//     case (2):
+//         console.log("February");
+//         break;
+//     case (3):
+//         console.log("March");
+//         break;
+//     case (4):
+//         console.log("April");
+//         break;
+//     case (5):
+//         console.log("Desember");
+//         break;
+//     case (6):
+//         console.log("Jun");
+//         break;
+//     case (7):
+//         console.log("July");
+//         break;
+//     case (8):
+//         console.log("Agust");
+//         break;
+//     case (9):
+//         console.log("September");
+//         break;
+//     case (10):
+//         console.log("October");
+//         break;
+//     case (11):
+//         console.log("November");
+//         break;
+//     case (12):
+//         console.log("Desember");
+//         break;
+//     default:
+//         console.log("Invalid value");
+//         break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// const num = prompt("введіть тризначне число: ");
+// if (num === (num[0] + num[1] + num[2]) ) {
+//     const sum =
+//         Number(num[0])+
+//         Number(num[1])+
+//         Number(num[2]);
+//     console.log(sum);
+// } else {
+//     console.log("invalid value");
+// }
+
+
+
+
+
